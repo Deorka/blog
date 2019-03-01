5c730caf91c16990cef48c95152f96993ec9423d
Alena Nidilko <alenkan96@bk.ru>
2019-03-01 18:28:24 +0300

    Modified templates to display posts from database

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User #я добавила вне инструкции
 
 
 class Post(models.Model):
